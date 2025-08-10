5f6b2b5b31f6397e31d9e93abae93da6c9c9e9aa
akgusgh <akgusgh1234@gmial.com>
2025-08-10 22:04:11 +0900

    25.08.10

---
+++ Chapter17/DuckTyping.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Chapter16
+{
+    class Duck
+    {
+        public void Walk() { Console.WriteLine(this.GetType()+" Walk"); }
+        public void Swim() { Console.WriteLine(this.GetType+" Swim"); }
+        public void Quack() {Console.WriteLine(this.GetType()+" Quack"); }
+    }
+    class Mallard:Duck { }
+    class Robot
+    {
+        public void Walk() { Console.WriteLine(this.GetType() + " Walk"); }
+        public void Swim() { Console.WriteLine(this.GetType() + " Swim"); }
+        public void Quack() { Console.WriteLine(this.GetType() + " Quack"); }
+    }
+    internal class DuckTyping
+    {
+        static void Main(string[] args)
+        {
+            dynamic[]arr=new dynamic[] {new Duck(),new Mallard(),new Robot()};
+            foreach (dynamic d in arr)
+            {
+                Console.WriteLine(d.GetType());
+                d.Walk();
+                d.Swim();
+                d.Quack();
+                Console.WriteLine();
+            }
+        }
+    }
+}
