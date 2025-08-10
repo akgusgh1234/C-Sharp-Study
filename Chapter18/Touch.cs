02fc9fc9fe0f31f31ea7137a5c75bfc9b63fccb6
akgusgh <akgusgh1234@gmial.com>
2025-08-10 22:14:34 +0900

    25.08.10

---
+++ Chapter18/Touch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+/*
+namespace Chapter18
+{
+    internal class Touch
+    {
+        static void OnWrongType(string type)
+        {
+            Console.WriteLine($"{type} is wrong type");
+            return;
+        }
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Usage : Touch.exe <Path> [Type:File/Directory]");
+                return;
+            }
+            string path=args[0];
+            string type = "File";
+            //나머지는 스킵
+        }
+    }
+}
+*/
